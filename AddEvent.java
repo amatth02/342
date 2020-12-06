625190434f4eacb9c1989224db41bd9a233be916
Georgia Andreou <gandre03@cs.ucy.ac.cy>
2020-12-06 10:29:26 +0200

    Update AddEvent.java

--- AddEvent.java
+++ AddEvent.java
@@ -53,7 +53,7 @@ public class AddEvent {
 	private void initialize() {
 		frmSocialface = new JFrame();
 		frmSocialface.setTitle("SocialFace");
-		frmSocialface.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\User\\eclipse-workspace\\EPL342\\src\\logo.jpg"));
+		frmSocialface.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Georgia\\Workspace\\EPL342\\src\\logo.jpg"));
 		frmSocialface.setBounds(100, 100, 441, 500);
 		frmSocialface.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmSocialface.getContentPane().setLayout(null);
