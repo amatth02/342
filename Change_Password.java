1411dbf7c4d581eb6f211295f1cde12d3ae4df90
Georgia Andreou <gandre03@cs.ucy.ac.cy>
2020-12-06 10:29:15 +0200

    Update Change_Password.java

--- Change_Password.java
+++ Change_Password.java
@@ -6,10 +6,11 @@ import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Toolkit;
 
 public class Change_Password {
 
-	private JFrame frame;
+	private JFrame frmSocialface;
 	private JPasswordField passwordField;
 	private JPasswordField passwordField_1;
 	private JPasswordField passwordField_2;
@@ -22,7 +23,7 @@ public class Change_Password {
 			public void run() {
 				try {
 					Change_Password window = new Change_Password();
-					window.frame.setVisible(true);
+					window.frmSocialface.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -41,38 +42,40 @@ public class Change_Password {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmSocialface = new JFrame();
+		frmSocialface.setTitle("SocialFace");
+		frmSocialface.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Georgia\\Workspace\\EPL342\\src\\logo.jpg"));
+		frmSocialface.setBounds(100, 100, 450, 300);
+		frmSocialface.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmSocialface.getContentPane().setLayout(null);
 		
 		JLabel label = new JLabel("Change password");
 		label.setBounds(54, 13, 166, 16);
-		frame.getContentPane().add(label);
+		frmSocialface.getContentPane().add(label);
 		
 		JLabel label_1 = new JLabel("old password");
 		label_1.setBounds(91, 77, 94, 16);
-		frame.getContentPane().add(label_1);
+		frmSocialface.getContentPane().add(label_1);
 		
 		passwordField = new JPasswordField();
 		passwordField.setBounds(241, 74, 101, 22);
-		frame.getContentPane().add(passwordField);
+		frmSocialface.getContentPane().add(passwordField);
 		
 		JLabel label_2 = new JLabel("new password");
 		label_2.setBounds(91, 109, 83, 16);
-		frame.getContentPane().add(label_2);
+		frmSocialface.getContentPane().add(label_2);
 		
 		passwordField_1 = new JPasswordField();
 		passwordField_1.setBounds(241, 106, 101, 22);
-		frame.getContentPane().add(passwordField_1);
+		frmSocialface.getContentPane().add(passwordField_1);
 		
 		JLabel label_3 = new JLabel("verified new password");
 		label_3.setBounds(91, 144, 129, 16);
-		frame.getContentPane().add(label_3);
+		frmSocialface.getContentPane().add(label_3);
 		
 		passwordField_2 = new JPasswordField();
 		passwordField_2.setBounds(241, 141, 101, 22);
-		frame.getContentPane().add(passwordField_2);
+		frmSocialface.getContentPane().add(passwordField_2);
 		
 		JButton button = new JButton("Save");
 		button.addActionListener(new ActionListener() {
@@ -82,7 +85,7 @@ public class Change_Password {
 			}
 		});
 		button.setBounds(114, 176, 202, 25);
-		frame.getContentPane().add(button);
+		frmSocialface.getContentPane().add(button);
 	}
 
 }
